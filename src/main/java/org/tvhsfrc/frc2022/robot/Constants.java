38659c3845ee6c65aa99e78ba6f2f6aa6d375020
Brian Rozmierski <brianr@warped-minds.com>
2022-01-14 17:05:39 -0500

    Swerve Drive Template and lib fixes... ugh

--- src/main/java/org/tvhsfrc/frc2022/robot/Constants.java
+++ src/main/java/org/tvhsfrc/frc2022/robot/Constants.java
@@ -13,4 +13,38 @@ package org.tvhsfrc.frc2022.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    /**
+     * The left-to-right distance between the drivetrain wheels
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.579;
+    /**
+     * The front-to-back distance between the drivetrain wheels.
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.57;
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 10;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 11;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 1;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front left steer offset
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 12;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 13;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 2;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front right steer offset
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 14;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 15;
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 3;
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back left steer offset
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 16;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 17;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 4;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back right steer offset
+
+}
